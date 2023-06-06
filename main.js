7b355cad5bccc28836ea4e171077bb236c7f6d34
Thayanne Lima <111809828+ThayanneLima@users.noreply.github.com>
2023-06-06 14:12:09 -0300

    Update main.js

--- main.js
+++ main.js
@@ -57,45 +57,7 @@ function myKeyDown(e)
 			console.log("direita");
 		}
 }
-function up()
-{
-	if(roverY >=0)
-	{
-		roverY = roverY - 10;
-		console.log("Quando direcional cima for pressionada,  x = " + roverX + " | y = " +roverY);
-		 uploadBackground();
-		 uploadrover();
-	}
-}
-function down()
-{
-	if(roverY <=500)
-	{
-		roverY =roverY+ 10;
-		console.log("Quando direcional baixo for pressionada,  x = " + roverX + " | y = " +roverY);
-		uploadBackground();
-		 uploadrover();
-	}
-}
-function left()
-{
-	if(roverX >= 0)
-	{
-		roverX =roverX - 10;
-		console.log("Quando direcional esquerda for pressionada,  x = " + roverX + " | y = " +roverY);
-		uploadBackground();
-		 uploadrover();
-	}
-}
-function right()
-{
-	if(roverX <= 700)
-	{
-		roverX =roverX + 10;
-		console.log("Quando direcional direita for pressionada,  x = " + roverX + " | y = " +roverY);
-		uploadBackground();
-		uploadrover();
-   }
-}
+
+
 
 	
